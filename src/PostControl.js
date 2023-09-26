b2506500940c92aed816777c9f0da3dc3fc5ad76
Khrystyna <k-r-i-s-t-i-n-a@i.ua>
2023-09-26 18:34:24 +0300

    toggle formVisible

--- src/PostControl.js
+++ src/PostControl.js
@@ -9,19 +9,29 @@ class PostControl extends React.Component {
     this.state = {
         formVisible: false
     };
+}
 
+    handleClick = () => {
+        this.setState(prevState =>  ({
+         formVisible: !prevState.formVisible
+        }));
     }
+
 
     render() {
         let currentlyVisible = null;
+        let buttonText = null;
         if (this.state.formVisible) {
           currentlyVisible = <NewPostForm/>
+          buttonText="To the posts";
         }
         else {
           currentlyVisible = <PostList />
+          buttonText="Create post";
         }
         return (
         <React.Fragment>
+         <button onClick={this.handleClick}>{buttonText}</button>
          {currentlyVisible}
         </React.Fragment>
         );
