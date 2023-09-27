7effa909b4bb16e6eb28fe2ca4a228122031f827
Khrystyna <k-r-i-s-t-i-n-a@i.ua>
2023-09-27 16:22:00 +0300

    newPostForm started.

--- src/NewPostForm.js
+++ src/NewPostForm.js
@@ -1,9 +1,28 @@
 import React from "react";
 
 function NewPostForm() {
+
+    function handleNewPostFormSubmission(event) {
+        event.preventDefault();
+    }
+
     return (
         <React.Fragment>
-            <h3>This will be form</h3>
+            <form onSubmit={handleNewPostFormSubmission}>
+                <input 
+                type="text"
+                name="text"
+                placeholder="What do you think?"/>
+                <input 
+                type="text"
+                name="imgUrl"
+                placeholder="Put image URL here"/>
+                <input 
+                type="text"
+                name="autor"
+                placeholder="What'your name?"/>
+                <button type="submit">Post!</button>
+            </form>
         </React.Fragment>
     );
 }
